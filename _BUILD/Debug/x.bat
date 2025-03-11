bf18601e488899fb88c4e5b093c92216ac2ee23e
Mike Weiblen <mew@mew.cx>
2025-03-10 23:57:54 -0600

    _BUILD/Debug/x.bat

---
+++ _BUILD/Debug/x.bat
@@ -0,0 +1,7 @@
+REM Path=C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\;C:\Windows\System32\OpenSSH\;C:\Program Files\dotnet\;C:\Program Files (x86)\Incredibuild;C:\Program Files (x86)\Windows Kits\10\Windows Performance Toolkit\;C:\Program Files\Git\cmd;C:\Program Files\CMake\bin;C:\Users\mew\.cargo\bin;C:\Users\mew\AppData\Local\Microsoft\WindowsApps;e:\usr\ap\osgearth_mew\vcpkg\installed\x64-windows\bin\;e:\usr\ap\osgearth_mew\vcpkg\installed\x64-windows\debug\bin\;e:\usr\ap\osgearth_mew\vcpkg\installed\x64-windows\debug\plugins\osgPlugins-3.6.5\;e:\usr\ap\osgearth_mew\vcpkg\installed\x64-windows\plugins\osgPlugins-3.6.5\;c:\Qt\6.8.2\msvc2022_64\bin\;
+
+Path=C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\;C:\Windows\System32\OpenSSH\;C:\Program Files\dotnet\;C:\Program Files (x86)\Incredibuild;C:\Program Files (x86)\Windows Kits\10\Windows Performance Toolkit\;C:\Program Files\Git\cmd;C:\Program Files\CMake\bin;C:\Users\mew\.cargo\bin;C:\Users\mew\AppData\Local\Microsoft\WindowsApps;e:\usr\ap\osgearth_mew\vcpkg\installed\x64-windows\bin\;e:\usr\ap\osgearth_mew\vcpkg\installed\x64-windows\debug\bin\;e:\usr\ap\osgearth_mew\vcpkg\installed\x64-windows\debug\plugins;e:\usr\ap\osgearth_mew\vcpkg\installed\x64-windows\plugins;c:\Qt\6.8.2\msvc2022_64\bin\;
+
+.\example-gl.exe
+.\example-osg.exe
+.\example-osgearth.exe
