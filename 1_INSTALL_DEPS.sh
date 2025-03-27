f2043bedadbfc68982f3497d91bf95240baeac94
Mike Weiblen <mew@mew.cx>
2025-03-26 23:43:36 -0600

    g rm 0_BOOTSTRAP_VCPKG.sh 1_INSTALL_DEPS.sh

--- 1_INSTALL_DEPS.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash -x
-
-cd vcpkg
-
-./bootstrap-vcpkg.sh
-time ./vcpkg install osgearth[tools]:x64-windows
-time ./vcpkg install qtbase:x64-windows
-time ./vcpkg install qttools:x64-windows
