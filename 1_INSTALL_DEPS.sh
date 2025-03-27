8eb5acf5cf1d408648a0c8f1d95cec45e157462a
Mike Weiblen <mew@mew.cx>
2025-03-26 23:35:03 -0600

    1$

--- 1_INSTALL_DEPS.sh
+++ 1_INSTALL_DEPS.sh
@@ -1,12 +1,6 @@
 #! /bin/bash -x
 
-git clone https://github.com/microsoft/vcpkg.git
 cd vcpkg
-git checkout 2025.01.13
-
-echo "set(osg_OPENGL_PROFILE GLCORE)" >> triplets/x64-windows.cmake
-#git status
-#git diff
 
 ./bootstrap-vcpkg.sh
 time ./vcpkg install osgearth[tools]:x64-windows
